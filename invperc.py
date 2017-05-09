9d8640c333ecc404dd16de164dc8a8009649184b
Eric Stansifer <eric.stansifer+git@gmail.com>
2017-05-08 21:01:41 -0400

    Added 2D case

--- invperc.py
+++ invperc.py
@@ -49,6 +49,48 @@ def invperc(L):
 
     return np.sum(visited[1:, :, :])
 
+def invperc2(L):
+    L = int(L)
+
+    adj = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+
+    visited = np.zeros((L + 1, L), dtype = bool)
+
+    rs = npr.random((6 * (L + 1) * L,))
+
+    i = 0
+    q = []
+    for y in range(L):
+        q.append((0, 0, y))
+
+    while len(q) > 0:
+        p, x, y = pop(q)
+
+        if not visited[x, y]:
+            visited[x, y] = True
+
+            if x == L:
+                break
+
+            for dx, dy in adj:
+                x_ = x + dx
+                y_ = (y + dy) % L
+                if x_ > 0 and not visited[x_, y_]:
+                    push(q, (rs[i], x_, y_))
+                    i += 1
+
+    return visited
+
+def print_example():
+    v = invperc2(1000)
+    a, b = v.shape
+
+    with open('invperc_example', 'w') as f:
+        for j in range(b):
+            s = ' '.join([str(v[i, j].astype(int)) for i in range(a)])
+            f.write(s)
+            f.write('\n')
+
 usage=(
 """A program for simulating invasion percolation.
 Usage:
